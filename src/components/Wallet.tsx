8746f43bdfc0740cb48bf1735efce6cd2d3a6252
Natalia <mientuska00@gmail.com>
2024-02-17 19:05:35 +0100

    style: finished stylng wallet component

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,31 +1,53 @@
 export default function Wallet() {
   return (
-    <section className="grid grid-rows-4 grid-cols-6 w-screen max-h-screen">
-      <h1 className="flex justify-center items-center row-start-1 row-end-2 col-start-2 col-end-6 text-center text-2xl uppercase font-semibold">
-        wallet💵
+    <section className="grid grid-rows-6 grid-cols-11 w-screen max-h-screen min-w-fit">
+      {/* Greeting  */}
+      <h1 className="flex justify-center items-center row-start-1 row-end-2 col-start-2 col-end-4 text-center text-2xl font-semibold">
+        <h2>
+          Hello, <strong className="text-purple-500 ml-1"> user!</strong>
+        </h2>
       </h1>
-      <div className="row-start-2 row-end-4 col-start-2 col-end-4 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-md">
-        <h2 className="font-semibold uppercase">your current budget:</h2>
-        <p className="p-5">
-          2350<strong> PLN</strong>
-        </p>
-        <h2 className="font-semibold uppercase">expenses:</h2>
-        <ul className="p-1">
-          <li className="bg-purple-200 rounded-md p-0.5 m-2">food</li>
-          <li className="bg-yellow-200 rounded-md p-0.5 m-2">home</li>
-          <li className="bg-blue-200 rounded-md p-0.5 m-2">transport</li>
-          <li className="bg-pink-200 rounded-md p-0.5 m-2">health</li>
-          <li className="bg-green-200 rounded-md p-0.5 m-2">unexpected</li>
-        </ul>
+
+      {/* Dashboard  */}
+      <div className=" row-start-2 row-end-2 col-start-2 col-end-5 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <h2>Total income:</h2>
       </div>
-      <div className="row-start-2 row-end-3 col-start-4 col-end-5 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-md">
-        Add to wallet
+      <div className=" row-start-2 row-end-2 col-start-5 col-end-8 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <h2>Total expenses:</h2>
       </div>
-      <div className="row-start-2 row-end-3 col-start-5 col-end-6 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-md">
-        Remove from wallet
+      <div className=" row-start-2 row-end-2 col-start-8 col-end-11 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <h2>Total balance:</h2>
       </div>
-      <div className="row-start-3 row-end-4 col-start-4 col-end-6 bg-slate-50  p-5 m-4 border-2 border-slate-300 rounded-md">
-        <h2>See expenses</h2>
+      <div className="row-start-3 row-end-4 col-start-2 col-end-4 bg-purple-100 p-3 m-4 border-2 border-purple-300 rounded-xl">
+        <h2 className="text-purple-600">wallet:</h2>
+      </div>
+      <div className="row-start-3 row-end-4 col-start-4 col-end-6 bg-green-100 p-3 m-4 border-2 border-green-300 rounded-xl">
+        <h2 className="text-green-600">savings:</h2>
+      </div>
+
+      {/* Icons  */}
+      <div className="flex items-center justify-center row-start-3 row-end-4 col-start-6 col-end-7 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <img src="/public/icons/food.svg" />
+      </div>
+      <div className="flex items-center justify-center row-start-3 row-end-4 col-start-7 col-end-8 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <img src="/public/icons/health.svg" />
+      </div>
+      <div className="flex items-center justify-center row-start-3 row-end-4 col-start-8 col-end-9 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <img src="/public/icons/home.svg" />
+      </div>
+      <div className="flex items-center justify-center row-start-3 row-end-4 col-start-9 col-end-10 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <img src="/public/icons/pets.svg" />
+      </div>
+      <div className="flex items-center justify-center row-start-3 row-end-4 col-start-10 col-end-11 bg-slate-50 p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <img src="/public/icons/others.svg" />
+      </div>
+
+      {/* Stats  */}
+      <div className="row-start-4 row-end-6 col-start-2 col-end-6 bg-slate-50  p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <h2>Incomes:</h2>
+      </div>
+      <div className="row-start-4 row-end-6 col-start-6 col-end-11 bg-slate-50  p-5 m-4 border-2 border-slate-300 rounded-xl">
+        <h2>Expenses:</h2>
       </div>
     </section>
   );
