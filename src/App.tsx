82af608a0fc258ea6876c4a8491b1533cd5d1eaf
Natalia Zagórska <vermenea@gmail.com>
2024-02-26 12:26:47 +0100

    fix: fixed issue with unknown path

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Wallet from "./components/Wallet";
 import Header from "./components/Header";
-import Stats from "./components/Stats";
+// import Stats from "./components/Stats";
 import Settings from "./components/Settings";
 
 function App() {
   return (
-    <div className="flex bg-slate-200">
+    <main className="flex bg-slate-200">
       <Router>
         <Nav />
         <Routes>
           <Route path="/" Component={Header} />
           <Route path="/wallet" Component={Wallet} />
-          <Route path="/stats" Component={Stats} />
+          {/* <Route path="/stats" Component={Stats} /> */}
           <Route path="/settings" Component={Settings} />
         </Routes>
       </Router>
-    </div>
+    </main>
   );
 }
 
